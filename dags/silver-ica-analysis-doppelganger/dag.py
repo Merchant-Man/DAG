b634600078ae520fa5242a35c4a5e5cff5f3685d
satriobio <satrio.biology@yahoo.com>
2024-10-02 23:22:31 +0100

    update dag

---
+++ dags/silver-ica-analysis-doppelganger/dag.py
@@ -0,0 +1,132 @@
+from datetime import datetime, timedelta
+import pandas as pd
+import io
+import ast
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.models import Variable
+
+S3_DWH_BRONZE=Variable.get("S3_DWH_BRONZE")
+S3_DWH_SILVER=Variable.get("S3_DWH_SILVER")
+prefix="ica/analysis/"
+
+default_args = {
+    'owner': 'data',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 9, 30),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+
+dag = DAG(
+    'silver-ica-analysis-doppelganger',
+    default_args=default_args,
+    description='ETL pipeline to merge CSV files from S3',
+    schedule_interval=timedelta(days=1),
+)
+
+def fetch_data(**kwargs):
+    s3 = S3Hook(aws_conn_id='aws')
+    
+    # List all objects in the S3 prefix
+    files = s3.list_keys(bucket_name=S3_DWH_BRONZE, prefix=prefix)
+    
+    if not files:
+        raise ValueError(f"No files found in {prefix}")
+    
+    all_data_frames = []
+
+    for file_key in files:
+        if file_key.endswith('.csv'):
+            # Read each CSV file into a DataFrame
+            csv_obj = s3.get_key(key=file_key, bucket_name=S3_DWH_BRONZE)
+            df = pd.read_csv(io.BytesIO(csv_obj.get()['Body'].read()))
+            all_data_frames.append(df)
+
+    # Merge all DataFrames into one
+    merged_df = pd.concat(all_data_frames, ignore_index=True)
+
+    # Convert merged DataFrame to CSV format
+    csv_buffer = io.StringIO()
+    merged_df.to_csv(csv_buffer, index=False)
+
+    return csv_buffer.getvalue()
+
+def transform_data(merged_data: str, **kwargs):
+    # Read the CSV data into a DataFrame
+    df = pd.read_csv(io.StringIO(merged_data))
+
+    # Remove duplicates
+    df = df.drop_duplicates()
+
+    # Clean up
+    df['id_repository'] = df['userReference'].str.split('_').str[0]
+    df['id_batch']      = df['tags'].apply(lambda x: ast.literal_eval(x)['userTags'][4] if len(ast.literal_eval(x)['userTags']) > 4 else None)
+    df['date_start']    = df['startDate']
+    df['date_end']      = df['endDate']
+    df['pipeline_name'] = df['pipeline'].apply(lambda x: ast.literal_eval(x)['code'])
+    df['pipeline_type'] = 'secondary'
+    df['run_name']      = df['userReference']
+    df['run_status']    = df['status']
+    df                  = df[['id_repository', 'id_batch', 'date_start', 'date_end', 'pipeline_name', 'pipeline_type', 'run_name', 'run_status']].copy()
+
+    # Convert cleaned DataFrame to CSV format
+    csv_buffer = io.StringIO()
+    df.to_csv(csv_buffer, index=False)
+
+    return csv_buffer.getvalue()
+
+def upload_to_s3(cleaned_data, **kwargs):
+    # Use data_interval_start for timestamp
+    data_interval_start = kwargs['ti'].get_dagrun().data_interval_start
+    s3_key = f'{prefix}{data_interval_start.isoformat()}.csv'
+
+    # Use S3Hook to upload the cleaned CSV to S3
+    s3 = S3Hook(aws_conn_id='aws')
+
+    # Upload the file with timestamp in the name
+    s3.load_string(
+        string_data=cleaned_data,
+        key=s3_key,
+        bucket_name=S3_DWH_SILVER,
+        replace=True
+    )
+
+    # Upload the same file as 'latest.csv'
+    s3_key_latest = f'{prefix}latest.csv'
+    s3.load_string(
+        string_data=cleaned_data,
+        key=s3_key_latest,
+        bucket_name=S3_DWH_SILVER,
+        replace=True
+    )
+
+# Define tasks
+fetch_data_task = PythonOperator(
+    task_id='fetch_data',
+    python_callable=fetch_data,
+    dag=dag,
+)
+
+transform_data_task = PythonOperator(
+    task_id='transform_data',
+    python_callable=transform_data,
+    provide_context=True,  # To pass kwargs
+    op_kwargs={'merged_data': '{{ task_instance.xcom_pull(task_ids="fetch_data") }}'},
+    dag=dag,
+)
+
+upload_to_s3_task = PythonOperator(
+    task_id='upload_to_s3',
+    python_callable=upload_to_s3,
+    provide_context=True,  # To pass kwargs
+    op_kwargs={'cleaned_data': '{{ task_instance.xcom_pull(task_ids="transform_data") }}'},
+    dag=dag,
+)
+
+# Set task dependencies
+fetch_data_task >> transform_data_task >> upload_to_s3_task
