c530ebf4d7a8cb9c3677cd5ed5f970e875c12288
satriobio <satrio.biology@yahoo.com>
2024-10-17 08:48:14 +0100

    update dag

--- dags/loader-rds-fast/dag.py
+++ dags/loader-rds-fast/dag.py
@@ -15,6 +15,7 @@ S3_DWH_SILVER = Variable.get("S3_DWH_SILVER")
 keys = [
     'ica/samples/latest.csv',
     'ica/analysis/latest.csv',
+    'illumina/qs/latest.csv',
     'illumina/qc/latest.csv',
     'zlims/samples/latest.csv',
     'mgi/analysis/latest.csv',
