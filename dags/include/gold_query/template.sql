124755b6f1b4c4ab033d7af8824abfeaaee2e736
abdulfaqihalm <abdulfaqihalm@gmail.com>
2025-02-13 19:46:23 +0700

    feat: add gold_query DAG to store daily query for gold tables used for reporting

---
+++ dags/include/gold_query/template.sql
@@ -0,0 +1,11 @@
+/*
+---------------------------------------------------------------------------------------------------------------------------------
+-- Purpose:  This query is intended to be used as the source of QC metrics.
+-- Author:   Renata
+-- Created:  10-02-2025
+-- Modified|by: <Date of Last Modification> 
+-- Changes:  <Brief description of changes made in each modification, along with the person who modified it>
+---------------------------------------------------------------------------------------------------------------------------------
+*/
+
+-- Your SQL code goes here 
