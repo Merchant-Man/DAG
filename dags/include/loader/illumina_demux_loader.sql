0a8e5ea2b96081d16524eb99ef65cfdd36cb8f64
rtriwijaya <triwijaya.renata@gmail.com>
2025-08-26 17:52:46 +0700

    update: loader table names

--- dags/include/loader/illumina_demux_loader.sql
+++ dags/include/loader/illumina_demux_loader.sql
@@ -1,4 +1,4 @@
-INSERT INTO illumina_demux_runs (
+INSERT INTO illumina_demux (
     id_repository
     , lane
     , index_seq
@@ -10,9 +10,12 @@ INSERT INTO illumina_demux_runs (
     , percent_perfect_index_reads
     , percent_one_mismatch_index_reads
     , percent_two_mismatch_index_reads
+    , id_library
+    , created_at
+    , updated_at
 )
 
-VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
 
 ON DUPLICATE KEY UPDATE
 id_repository = VALUES(id_repository)
@@ -26,4 +29,5 @@ id_repository = VALUES(id_repository)
 , percent_perfect_index_reads = VALUES(percent_perfect_index_reads)
 , percent_one_mismatch_index_reads = VALUES(percent_one_mismatch_index_reads)
 , percent_two_mismatch_index_reads = VALUES(percent_two_mismatch_index_reads)
+, id_library = VALUES(id_library)
 , updated_at = '{{ ts }}'
