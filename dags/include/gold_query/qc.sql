495f80c5b089f6f0eccaf2853c3540a5c2beef1b
Ubuntu <ubuntu@ip-10-0-3-102.ap-southeast-3.compute.internal>
2025-09-09 03:54:41 +0000

    update cov qc

--- dags/include/gold_query/qc.sql
+++ dags/include/gold_query/qc.sql
@@ -199,8 +199,9 @@ WITH
 			CASE
 				WHEN coverage >= 30
 				AND at_least_10x >= 85 THEN 'Pass'
-				WHEN (coverage IS NULL OR coverage = '0')
-				AND (at_least_10x IS NULL OR at_least_10x = '0') THEN 'No Data'
+				# WHEN (coverage IS NULL OR coverage = 0)
+				WHEN coverage IS NULL AND (at_least_10x IS NULL OR at_least_10x = 0) THEN 'No Data'
+    				WHEN coverage = 0 AND (at_least_10x IS NULL OR at_least_10x = 0) THEN 'Fail'
 				ELSE 'Fail'
 			END AS coverage_category,
 
